013ccf0c0ceed542af34141179378f961b600fd9
vetlesen <jonas@vetlesen.no>
2024-09-10 23:55:44 +0200

    feat: server on arduino

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const parser = new parsers.Readline({
     delimiter: '\r\n'
 });
 
-var port = new SerialPort('/dev/tty.wchusbserialfa1410',{ 
+var port = new SerialPort('/dev/cu.usbmodem101', { 
     baudRate: 9600,
     dataBits: 8,
     parity: 'none',
@@ -20,31 +20,36 @@ var port = new SerialPort('/dev/tty.wchusbserialfa1410',{
 port.pipe(parser);
 
 var app = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(index);
+    if (req.url === '/style.css') {
+        fs.readFile('style.css', function(err, data) {
+            if (err) {
+                res.writeHead(404);
+                res.end('CSS file not found');
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/css'});
+                res.end(data);
+            }
+        });
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(index);
+    }
 });
 
 var io = require('socket.io').listen(app);
 
 io.on('connection', function(socket) {
-    
     console.log('Node is listening to port');
 
-    socket.on('lights',function(data){
-        
-        console.log( data );
-        port.write( data.status );
-    
+    socket.on('lights', function(data) {
+        console.log(data);
+        port.write(data.status);
     });
-    
 });
 
 parser.on('data', function(data) {
-    
     console.log('Received data from port: ' + data);
-    
     io.emit('data', data);
-    
 });
 
 app.listen(3000);
